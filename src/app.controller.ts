f841d457d99dacb6580b8f0d5d0e446537013b36
coche11 <colin.chen1@ingka.ikea.com>
2021-01-31 20:24:55 +0800

    生成pdf测试

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
-import { rename } from 'fs';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
+import { Response } from 'express';
+import { Readable } from 'stream';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,5 +12,40 @@ export class AppController {
   @Render('index')
   getHello() {
     return { message: this.appService.getHello() };
+  }
+
+  @Get('downloadPdf')
+  async downloadPdf(@Res() res: Response) {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const filename = `doc/test.pdf`;
+    const page = await browser.newPage();
+    const url = 'http://localhost:3000';
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const pdf = await page.pdf({
+      path: filename,
+      width: '1188px',
+      height: '840px',
+      scale: 1,
+      printBackground: true,
+      landscape: false,
+      displayHeaderFooter: false,
+    });
+    await browser.close();
+    const stream = new Readable();
+    stream.push(pdf);
+    stream.push(null);
+    /* 直接打开
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Length': pdf.length,
+    });*/
+    res.set({
+      'Content-Type': 'application/octet-stream',
+      'Content-Disposition': `attachment; filename=${filename}`,
+    });
+    stream.pipe(res);
   }
 }
